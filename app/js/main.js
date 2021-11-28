79b7efb559b4c31d46f039afaeca6fb087338d96
Dev1NA <andreykoandroshchuk@gmail.com>
2021-11-28 23:30:22 +0200

    created product list 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,29 @@
 $(function (){
+
+
+  $(".filter-price__input").ionRangeSlider({
+    onStart: function (data) {
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
+    },
+    onChange: function (data) {
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
+    }
+  });
+
+
+  $(".star").rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "8px",
+  });
+
+
+
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
