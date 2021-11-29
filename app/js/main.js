cf30b980f584d44c72b37f633020f240195d42f1
Dev1NA <andreykoandroshchuk@gmail.com>
2021-11-29 22:32:30 +0200

    created product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,44 @@
 $(function (){
 
 
+  $(".related-products__items").slick({
+    
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  });
+
+
+  $(".product-tabs__item").on("click", function (e) {
+    e.preventDefault();
+    $(".product-tabs__item").removeClass("product-tabs__item--active");
+    $(this).addClass("product-tabs__item--active");
+
+    $(".product-tabs__content-item").removeClass(
+      "product-tabs__content-item--active"
+    );
+    $($(this).attr("href")).addClass("product-tabs__content-item--active");
+  });
+
+
+  $(".product-one__input").styler();
+
+  $(".product-slide__thumb").slick({
+    asNavFor: ".product-slide__big",
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    vertical: true,
+    draggable: false,
+  });
+  $(".product-slide__big").slick({
+    asNavFor: ".product-slide__thumb",
+    draggable: false,
+    arrows: false,
+    fade: true,
+  });
+
+
   $(".filter-price__input").ionRangeSlider({
     onStart: function (data) {
       $(".filter-price__from").text(data.from);
@@ -20,7 +58,7 @@ $(function (){
     readOnly: true,
     spacing: "8px",
   });
-  $(".list2-item__star").rateYo({
+  $(".list2-item__star, .product-one__star").rateYo({
     starWidth: "20px",
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00",
